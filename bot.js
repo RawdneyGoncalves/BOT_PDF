7d5eb43c7562032f283c2a084fc49e7c1f3e80ee
RawdneyMendes <53352051+RawdneyGoncalves@users.noreply.github.com>
2022-02-08 04:48:15 -0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,42 @@
+const axios = require('axios').default
+const cheerio = require('cheerio')
+const fs = require('fs')
+const download = require('download-pdf')
+
+
+//consumo API
+
+const url = 'https://www.spdo.ms.gov.br/diariodoe'
+
+function getDOE() {
+  axios(url)
+    .then(response => {
+      const html = response.data
+      const $ = cheerio.load(html)
+      let link = []
+      const endereco = 'https://www.spdo.ms.gov.br/diariodoe/Index/Download/'
+      $('#tbDiarios > tbody > tr', html).each(function () {
+        const linkDOE = $(this).find('a').attr('id')
+        link.push(
+          endereco + linkDOE
+        )
+      })
+      link.forEach((url, linkDoe) => {
+        let options = {
+          directory: __dirname,
+          filename: linkDoe + '.pdf'
+        }
+        download(url, options, function (err) {
+          if (err) throw err
+          console.log('baixado')
+        })
+
+      })
+
+
+      //console.log(link)
+
+    })
+}
+
+getDOE()
